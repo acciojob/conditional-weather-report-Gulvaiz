a79b479f5081015015d56b4b8939007b53d47bbe
Gulvaiz <gulvaizalam12@gmail.com>
2024-11-03 16:16:46 +0530

    update

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,19 +1,14 @@
-import React, { useState } from 'react'
-
 const WeatherDisplay = ({data}) => {
   
- const {temperature , conditions} = data
- const color = temperature > 20 ? "red" : "blue"
-
-    return (
-    <div>
-       <p style ={{color}}>temperature : {temperature}</p>
-       <p>condition: {conditions}</p>
-    </div>
-
-
-)
-}
-
-export default WeatherDisplay
- 
+    const {temperature , conditions} = data
+    const color = temperature > 20 ? "red" : "blue"
+   
+       return (
+       <div>
+          <p style ={{color}}><span>Temperature: {temperature}</span></p>
+          <p><span>Conditions: {conditions}</span></p>
+       </div>
+   )
+   }
+   
+   export default WeatherDisplay
